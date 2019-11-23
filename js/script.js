b75b5f4a3fba2af04e36a47f24bb2df6f5a858fe
Michał Grzegorczyk <michalgrzegorczyk4@gmail.com>
2019-11-23 20:33:10 +0100

    Add playerMove

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
+
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -23,3 +24,18 @@ if (randomNumber == '1') {
 printMessage();
 
 'Mój ruch: ' + computerMove;
+
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
